62b31ca1231a5f338d1848a1d1b5a201d7142ec5
NMekks <nusaibamekka@gmail.com>
2025-08-07 22:54:12 +0300

    incomplete work of collapsible sidebar, to be continued

---
+++ index.js
@@ -0,0 +1,11 @@
+const sidebar = document.getElementById("sidebar");
+const main = document.getElementById("main");
+const navText = document.getElementById("nav-text");
+
+function openNav() {
+    sidebar.style.width = "20%";
+}
+
+function closeNav() {
+    sidebar.style.width = "5%";
+}
